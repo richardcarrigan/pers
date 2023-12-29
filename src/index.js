039481629ebb973cf0ab7449d880662c33a03b27
Richard Carrigan <me@richardcarrigan.com>
2023-12-28 22:03:35 -0800

    Adds Vercel analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Analytics } from '@vercel/analytics';
 
 import './index.css';
 import App from './App';
@@ -15,13 +16,16 @@ const container = document.getElementById('app');
 const root = createRoot(container);
 
 root.render(
-  <Auth0Provider
-    domain='dev-4d6u7snvyprxooqo.us.auth0.com'
-    clientId='XPGKdNsp1T9ZvwQ69a8G30aQYdhZ0aNT'
-    redirectUri={`${window.location.origin}/dashboard`}
-  >
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Auth0Provider>
+  <>
+    <Auth0Provider
+      domain='dev-4d6u7snvyprxooqo.us.auth0.com'
+      clientId='XPGKdNsp1T9ZvwQ69a8G30aQYdhZ0aNT'
+      redirectUri={`${window.location.origin}/dashboard`}
+    >
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Auth0Provider>
+    <Analytics />
+  </>
 );
